cca96c0a157976b64529ab761ac263853368f022
Alex Moroz <morozo.alexey2003@gmail.com>
2023-10-25 00:23:35 +0300

    add page load time

---
+++ js/loadTime.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', () => {
+    const [p] = performance.getEntriesByType("navigation");
+    document.getElementById('load-time').innerHTML = (p.domComplete.toFixed()).toString()
+});
