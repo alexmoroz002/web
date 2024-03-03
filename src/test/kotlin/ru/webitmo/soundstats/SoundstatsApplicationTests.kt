960e93ce34b554e750eda0ad1bffb94b999b0a99
Alex Moroz <morozo.alexey2003@gmail.com>
2024-03-03 11:50:26 +0300

    move to spring

---
+++ src/test/kotlin/ru/webitmo/soundstats/SoundstatsApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.webitmo.soundstats
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SoundstatsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
