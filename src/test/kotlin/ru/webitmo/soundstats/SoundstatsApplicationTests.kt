4b86d2e3e6472422bc74bcbfec00e6f1a056de03
Alex Moroz <morozo.alexey2003@gmail.com>
2024-03-26 03:27:03 +0300

    ignore tests on deploy

--- src/test/kotlin/ru/webitmo/soundstats/SoundstatsApplicationTests.kt
+++ src/test/kotlin/ru/webitmo/soundstats/SoundstatsApplicationTests.kt
@@ -2,8 +2,10 @@ package ru.webitmo.soundstats
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SoundstatsApplicationTests {
 
 	@Test
