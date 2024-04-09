f60a97e8e8ed7337fcfb00ca6907b07381b31f47
Alex Moroz <morozo.alexey2003@gmail.com>
2024-04-10 01:47:40 +0300

    change db script

--- src/main/resources/v1__init.sql
+++ src/main/resources/v1__init.sql
@@ -1,12 +1,9 @@
 create table if not exists users
 (
---     id bigserial primary key not null,
-    spotify_id varchar(50) primary key not null,
-    name varchar(50),
-    country char(2),
-    is_explicit bool,
-    avatar_url varchar(100),
-    product varchar(20),
-    access_token varchar(256),
-    refresh_token varchar(256)
-)
+    spotify_id           varchar(50) not null primary key,
+    user_name            varchar(50),
+    country              char(2),
+    is_explicit_filtered boolean,
+    avatar_url           varchar(100),
+    product              varchar(20)
+);
